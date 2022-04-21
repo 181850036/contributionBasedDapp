65a76516191acadd95b142a3f3575ce4911a6873
chaos <164851545@qq.com>
2022-04-21 21:56:51 +0800

    feat: voting

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -9,37 +9,57 @@ contract voting{
     mapping (bytes32 => uint ) public contributionReceived; // 收到的贡献度
     string public target; // 记录投票内容
     bytes32[] public optionList; // 投票选项
-    uint private _deadline; // 投票中止时间
+    uint private deadline; // 投票中止时间
     address owner; // 构建者
+    mapping (address => bool) public hasVoted;// 判断是否已经投过票 默认初始值为false
 
     // 构造函数
     constructor(string memory _target, uint _hoursAfter, bytes32[] memory _optionList){
         target = _target;
-        _deadline = block.timestamp + _hoursAfter * 1 hours;
+        deadline = block.timestamp + _hoursAfter * 1 hours;
         optionList = _optionList;
         owner = msg.sender;
     }
 
-    // 限制当前时间应当早于中止时间
+    // 限制-当前时间应当早于中止时间
     modifier notExpired(){
         require(
-            block.timestamp <= _deadline,
+            block.timestamp <= deadline,
             "To Late, the vote is over."
         ); 
         _;
     }
 
-    // 查询目标所收到的所有贡献度
-    function totalContributionFor(bytes32 _option) view public returns(uint){
-        return contributionReceived[_option];
-    } 
+    // 限定为持有者调用
+    modifier onlyOwner(){
+        require(
+            msg.sender == owner,
+            "Only owner can call this function"
+        );
+        _;
+    }
+
+    // 判断用户是否有权利投票
+    modifier canVote(){
+        require(
+            hasVoted[msg.sender] = false,
+            "You have voted and cannot vote again."
+        );
+        _;
+        hasVoted[msg.sender] = true;
+    }
 
     // 向目标选项投票
-    function voteForTarget(bytes32 _option) public notExpired {
+    function voteForTarget(bytes32 _option) public notExpired canVote{
         uint _index = indexOfOption(_option);
         require( _index != optionList.length );
         contributionReceived[_option] += contributorInfo[msg.sender].contribution;
     }
+
+    // 获取目标所收到的所有贡献度
+    function totalContributionFor(bytes32 _option) view public returns(uint){
+        return contributionReceived[_option];
+    } 
 
     // 获取投票选项中目标投票的下标
     function indexOfOption(bytes32 _option) view public returns(uint){
@@ -57,17 +77,8 @@ contract voting{
     }
 
     // 按次获取所有投票选项
-    function allOptions() view public returns(bytes32[] memory){
+    function getAllOptions() view public returns(bytes32[] memory){
         return optionList;
-    }
-
-    // 限定为持有者调用
-    modifier onlyOwner(){
-        require(
-            msg.sender == owner,
-            "Only owner can call this function"
-        );
-        _;
     }
 
     // 回收程序
